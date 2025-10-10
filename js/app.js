cf8f4a6efa733bd74a9d61c5477b549a0f3e2a6e
Damaris Poppe <damariscecilia29@gmail.com>
2025-10-09 23:26:57 -0600

    ✨ Feat: Implement conditional ui change on navbar target click

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
-const updateUI = () => {
+const updateViewNav = (View) => {
+    const navHome = document.getElementById('nav-home');
+    const navCheckin = document.getElementById('nav-check-in');
+    const navMonthly = document.getElementById('nav-monthly');
+    const navProjects = document.getElementById('nav-projects');
+    const navBookshelf = document.getElementById('nav-bookshelf');
+
+    View == 'Home' ? navHome.classList.add('active-nav') : navHome.classList.remove('active-nav');
+    View == 'Checkin' ? navCheckin.classList.add('active-nav') : navCheckin.classList.remove('active-nav');
+    View == 'Monthly' ? navMonthly.classList.add('active-nav') : navMonthly.classList.remove('active-nav');
+    View == 'Projects' ? navProjects.classList.add('active-nav') : navProjects.classList.remove('active-nav');
+    View == 'Bookshelf' ? navBookshelf.classList.add('active-nav') : navBookshelf.classList.remove('active-nav');
+}
+
+const updateInitialUI = () => {
     const savedUsername = localStorage.getItem('userName');
     const nameModal = document.getElementById('nameModal');
     const header = document.querySelector('header');
@@ -34,11 +48,25 @@ const handleNameSubmission = () => {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    updateUI();
+    updateInitialUI();
 
     // Start Button Listener (Modal)
     const startBtn = document.querySelector('#saveNameBtn');
     startBtn.addEventListener('click', handleNameSubmission);
+
+    // Navigation Listeners
+    const navHome = document.querySelector('#nav-home');
+    const navCheckin = document.querySelector('#nav-check-in');
+    const navMonthly = document.querySelector('#nav-monthly');
+    const navProjects = document.querySelector('#nav-projects');
+    const navBookshelf = document.querySelector('#nav-bookshelf');
+
+      navHome.addEventListener('click', () => updateViewNav('Home'));
+    navCheckin.addEventListener('click', () => updateViewNav('Checkin'));
+    navMonthly.addEventListener('click', () => updateViewNav('Monthly'));
+    navProjects.addEventListener('click', () => updateViewNav('Projects'));
+    navBookshelf.addEventListener('click', () => updateViewNav('Bookshelf'));
+
 })
 
 
